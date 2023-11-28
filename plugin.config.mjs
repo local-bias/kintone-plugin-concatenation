d511afe3c1e6a0e46e83dd728cd2c99e05fe6285
Ribbit <echo07200503@gmail.com>
2023-11-28 14:30:14 +0900

    🚀 Update plugin version to 1.1.0

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -10,7 +10,7 @@ export default {
   manifest: {
     base: {
       manifest_version: 1,
-      version: '1.0.0',
+      version: '1.1.0',
       type: 'APP',
       name: {
         en: 'String Concatenation Plugin',
